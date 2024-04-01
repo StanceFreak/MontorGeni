3a00e9c32f2ff37805ff8de14a394237060e6268
StanceFreak <jombor.army67@gmail.com>
2024-04-01 16:09:03 +0700

    [feat, refactors][api scrapers]: added new function to store memory util and network latency to the db and also added try catch for the error handling

--- scrapers/scrapers-api.js
+++ scrapers/scrapers-api.js
@@ -9,25 +9,142 @@ pool.on("error", (err) => {
 })
 
 async function storeCpuUtils(req, res, next) {
-    const objValues = {}
-    const url = `${ROOT_URL}/query`
-    const cpuUtil = await axios.get(url, {params: {query: '100 - (avg by (instance) (rate(node_cpu_seconds_total{job="node_exporter",mode="idle"}[5m])) * 100)'}})
-    cpuUtil.data.data.result.map((data) => {
-        const unixTime = new Date(data.value[0] * 1000)
-        const d = new Date()
-        objValues.CpuUtil = data == null ? 0:data.value[1]
-        objValues.time = data == null ? d.toLocaleTimeString() : unixTime.toLocaleTimeString()
-    })
-    const cpuValues = Object.values(objValues)
-    let data = {
-        cpuUtil: cpuValues[0],
-        time: cpuValues[1]
+    try {
+        const objValues = {}
+        const url = `${ROOT_URL}/query`
+        const cpuUtil = await axios.get(url, {params: {query: '100 - (avg by (instance) (rate(node_cpu_seconds_total{job="node_exporter",mode="idle"}[1m])) * 100)'}})
+        cpuUtil.data.data.result.map((data) => {
+            const unixTime = new Date(data.value[0] * 1000)
+            objValues.value = data.value[1]
+            objValues.time = unixTime.toLocaleTimeString('en-GB')
+        })
+        const cpuValues = Object.values(objValues)
+        let cpuData = {
+            value: cpuValues[0],
+            time: cpuValues[1]
+        }
+        pool.getConnection(function (err, conn) {
+            if (err) throw err
+            conn.query(`INSERT INTO cpu_util SET ?;`, [cpuData])
+            conn.release()
+        })
+    } catch (error) {
+        const objValues = {}
+        const unixTime = new Date()
+        objValues.value = 0.0
+        objValues.time = unixTime.toLocaleTimeString('en-GB')
+        const cpuValues = Object.values(objValues)
+        let emptyData = {
+            value: cpuValues[0],
+            time: cpuValues[1]
+        }
+        pool.getConnection(function (err, conn) {
+            if (err) throw err
+            conn.query(`INSERT INTO cpu_util SET ?;`, [emptyData])
+            conn.release()
+        })
+        console.log(`Server error: ${error.message}`)
     }
-    pool.getConnection(function (err, conn) {
-        if (err) throw err
-        conn.query(`INSERT INTO cpu_util SET ?;`, [data])
-        conn.release()
-    })
 }
 
-module.exports = storeCpuUtils
+async function storeMemUtils() {
+    try {
+        const objValues = {}
+        const url = `${ROOT_URL}/query`
+        const avgMemTotal = await axios.get(url, {params: {query: 'avg_over_time(node_memory_MemTotal_bytes{instance="localhost:9100"}[1m])'}})
+        const avgMemFree = await axios.get(url, {params: {query: 'avg_over_time(node_memory_MemFree_bytes{instance="localhost:9100"}[1m])'}})
+        const avgMemCached = await axios.get(url, {params: {query: 'avg_over_time(node_memory_Cached_bytes{instance="localhost:9100"}[1m])'}})
+        const avgMemBuffers = await axios.get(url, {params: {query: 'avg_over_time(node_memory_Buffers_bytes{instance="localhost:9100"}[1m])'}})
+        avgMemTotal.data.data.result.map((data) => {
+            const calc = parseInt(data.value[1])
+            objValues.memTotals = calc
+        })
+        avgMemFree.data.data.result.map((data) => {
+            const calc = parseInt(data.value[1])
+            objValues.memFree = calc
+        })
+        avgMemCached.data.data.result.map((data) => {
+            const calc = parseInt(data.value[1])
+            objValues.memCached = calc
+        })
+        avgMemBuffers.data.data.result.map((data) => {
+            const calc = parseInt(data.value[1])  
+            const unixTime = new Date(data.value[0] * 1000)
+            objValues.memBuffers = calc
+            objValues.date = unixTime.toLocaleTimeString('en-GB')
+        })
+        const memValues = Object.values(objValues)
+        const avgServerMemory = 100 * (1- ((memValues[1] + memValues[2] + memValues[3]) / memValues[0]))
+        let memData = {
+            value: avgServerMemory.toFixed(2),
+            time: memValues[4]
+        }
+        pool.getConnection(function (err, conn) {
+            if (err) throw err
+            conn.query(`INSERT INTO mem_util SET ?;`, [memData])
+            conn.release()
+        })
+    } catch (error) {
+        const objValues = {}
+        const unixTime = new Date()
+        objValues.value = 0.0
+        objValues.time = unixTime.toLocaleTimeString('en-GB')
+        const cpuValues = Object.values(objValues)
+        let emptyData = {
+            value: cpuValues[0],
+            time: cpuValues[1]
+        }
+        pool.getConnection(function (err, conn) {
+            if (err) throw err
+            conn.query(`INSERT INTO mem_util SET ?;`, [emptyData])
+            conn.release()
+        })
+        console.log(`Server error: ${error.message}`)
+    }
+}
+
+async function storeNetLatency(req, res, next) {
+    try {
+        const url = `${ROOT_URL}/query`
+        const objValues = {}
+        const serverLatency = await axios.get(url, {params: {query: 'sum(probe_icmp_duration_seconds{phase="rtt", instance="10.33.109.200"}) * 1000'}})
+        serverLatency.data.data.result.map((data) => {
+            const latency = parseFloat(data.value[1]).toFixed(1)
+            const unixTime = new Date(data.value[0] * 1000)
+            objValues.value = latency
+            objValues.date = unixTime.toLocaleTimeString('en-GB')
+        })
+        const latencyValues = Object.values(objValues)
+        let latencyData = {
+            value: latencyValues[0],
+            time: latencyValues[1]
+        }
+        pool.getConnection(function (err, conn) {
+            if (err) throw err
+            conn.query(`INSERT INTO net_latency SET ?;`, [latencyData])
+            conn.release()
+        })
+    } catch (error) {
+        const objValues = {}
+        const unixTime = new Date()
+        objValues.value = 0.0
+        objValues.time = unixTime.toLocaleTimeString('en-GB')
+        const latencyValues = Object.values(objValues)
+        let emptyData = {
+            value: latencyValues[0],
+            time: latencyValues[1]
+        }
+        pool.getConnection(function (err, conn) {
+            if (err) throw err
+            conn.query(`INSERT INTO net_latency SET ?;`, [emptyData])
+            conn.release()
+        })
+        console.log(`Server error: ${error.message}`)
+    }
+}
+
+module.exports = {
+    storeCpuUtils,
+    storeMemUtils,
+    storeNetLatency
+}
