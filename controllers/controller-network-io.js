329ca73fa298cf05fb370ca9921fe734c0c136fb
StanceFreak <muhammadraihanasyqar@gmail.com>
2024-04-05 16:42:19 +0700

    changes[calculation]: changed the calculation for the bytes to mebibytes conversion and added the locale time format

--- controllers/controller-network-io.js
+++ controllers/controller-network-io.js
@@ -6,7 +6,7 @@ async function getServerNetworkIo(req, res, next) {
         let tempApiResponse = []
         let apiResponse = []
         const url = `${ROOT_URL}/query`
-        const nwReceive = await axios.get(url, {params: {query : 'rate(otel_system_network_io_bytes_total[1m]) * 0.008'}})
+        const nwReceive = await axios.get(url, {params: {query : 'rate(otel_system_network_io_bytes_total[1m])/1048576'}})
         nwReceive.data.data.result.map((data) => {
             tempApiResponse.push({data})
         })
@@ -14,9 +14,10 @@ async function getServerNetworkIo(req, res, next) {
             const unixTime = new Date(tempApiResponse[item].data.value[0] * 1000)
             apiResponse.push({
                 device: tempApiResponse[item].data.metric.device,
+                direction: tempApiResponse[item].data.metric.direction,
                 type: tempApiResponse[item].data.metric.direction,
                 value: parseFloat(tempApiResponse[item].data.value[1]),
-                time: unixTime.toLocaleTimeString()
+                time: unixTime.toLocaleTimeString('en-GB')
             })
         }
         return res.status(200).json({
