4cf8e3c51c63fce1ae28e41d5949b5f8181532b9
StanceFreak <muhammadraihanasyqar@gmail.com>
2024-04-05 16:38:27 +0700

    changes[naming] & feat[cpu util]: changing the function name for retrieving data from db and added new function that retrieve data directly from query

--- controllers/controller-cpu-util.js
+++ controllers/controller-cpu-util.js
@@ -1,3 +1,5 @@
+const axios = require('axios')
+const {ROOT_URL} = require('../utils/options')
 const db = require('../utils/config-db')
 const mysql = require('mysql')
 const pool = mysql.createPool(db)
@@ -6,7 +8,7 @@ pool.on("error", (err) => {
     console.error(err)
 })
 
-async function getServerCpuUtil(req, res, next) {
+async function getServerCpuUtilRecord(req, res, next) {
     try {
         pool.getConnection(function (err, conn) {
             if (err) throw err
@@ -34,4 +36,33 @@ async function getServerCpuUtil(req, res, next) {
     }
 }
 
-module.exports = getServerCpuUtil
+async function getServerCpuUtil(req, res, next) {
+    try {
+        const objValues = {}
+        const url = `${ROOT_URL}/query`
+        const cpuUtil = await axios.get(url, {params: {query: '100 - (avg by (instance) (rate(node_cpu_seconds_total{job="node-exporter", mode="idle"}[1m])) * 100)'}})
+        cpuUtil.data.data.result.map((data) => {
+            const unixTime = new Date(data.value[0] * 1000)
+            objValues.value = data.value[1]
+            objValues.time = unixTime.toLocaleTimeString('en-GB')
+        })
+        const cpuValues = Object.values(objValues)
+        let cpuData = {
+            value: cpuValues[0],
+            time: cpuValues[1]
+        }
+        return res.status(200).json({
+            status: 200,
+            message: "success",
+            data: cpuData
+        })
+    } catch (error) {
+        res.status(400)
+        next(Error(error.message))
+    }
+}
+
+module.exports = {
+    getServerCpuUtilRecord,
+    getServerCpuUtil
+}
