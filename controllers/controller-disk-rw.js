5ea1fbf9614918d902c1f671ef25e4d02e49536b
StanceFreak <muhammadraihanasyqar@gmail.com>
2024-04-13 22:39:09 +0700

    feat[disk api]: added new function to get all of the disk read & write information and changing the query of the existing function

--- controllers/controller-disk-rw.js
+++ controllers/controller-disk-rw.js
@@ -1,12 +1,12 @@
 const axios = require('axios')
 const {ROOT_URL} = require('../utils/options')
 
-async function getServerDiskRw(req, res, next) {
+async function getServerDiskUtil(req, res, next) {
     try {
         let tempApiResponse = []
         let apiResponse = []
         const url = `${ROOT_URL}/query`
-        const diskRw = await axios.get(url, {params: {query: 'rate(otel_system_disk_io_bytes_total{device="sda"}[1m])/1048576 or rate(otel_system_disk_io_bytes_total{device="sr0"}[1m])/1048576'}})
+        const diskRw = await axios.get(url, {params: {query: 'rate(otel_system_disk_io_bytes_total{device=~".*sda.*"}[2m])/1048576 or rate(otel_system_disk_io_bytes_total{device="sr0"}[2m])/1048576'}})
         diskRw.data.data.result.map((data) => {
             tempApiResponse.push({data})
         })
@@ -16,7 +16,7 @@ async function getServerDiskRw(req, res, next) {
                 device: tempApiResponse[item].data.metric.device,
                 direction: tempApiResponse[item].data.metric.direction,
                 value: parseFloat(tempApiResponse[item].data.value[1]),
-                date: unixTime.toLocaleTimeString('en-GB')
+                time: unixTime.toLocaleTimeString('en-GB')
             })
         }
         return res.status(200).json({
@@ -30,4 +30,35 @@ async function getServerDiskRw(req, res, next) {
     }
 }
 
-module.exports = getServerDiskRw
+async function getServerDiskRw(req, res, next) {
+    try {
+        let tempApiResponse = []
+        let apiResponse = []
+        const url = `${ROOT_URL}/query`
+        const diskRw = await axios.get(url, {params: {query: 'sum by (direction)(rate(otel_system_disk_io_bytes_total[2m])/1048576)'}})
+        diskRw.data.data.result.map((data) => {
+            tempApiResponse.push({data})
+        })
+        for(item in tempApiResponse) {
+            const unixTime = new Date(tempApiResponse[item].data.value[0]*1000)
+            apiResponse.push({
+                direction: tempApiResponse[item].data.metric.direction,
+                value: parseFloat(tempApiResponse[item].data.value[1]).toFixed(1),
+                time: unixTime.toLocaleTimeString('en-GB')
+            })
+        }
+        return res.status(200).json({
+            status: 200,
+            message: "success",
+            data: apiResponse
+        })
+    } catch (error) {
+        res.status(400)
+        next(Error(error.message))
+    }
+}
+
+module.exports = {
+    getServerDiskUtil,
+    getServerDiskRw
+}
