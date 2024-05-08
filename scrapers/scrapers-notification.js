6d7e197b8f84fd8b6cab1ccd3fabf8e927f85f80
StanceFreak <jombor.army67@gmail.com>
2024-05-08 17:51:05 +0700

    feat[notification]: added new notification system for syncing with the database record

---
+++ scrapers/scrapers-notification.js
@@ -0,0 +1,158 @@
+const axios = require('axios')
+const {ROOT_URL} = require('../utils/options')
+const db = require('../utils/config-db')
+const mysql = require('mysql')
+const pool = mysql.createPool(db)
+const admin = require('firebase-admin')
+
+admin.initializeApp({
+    credential: admin.credential.cert('./serviceAccountKey.json')
+})
+
+async function notifScraper(type) {
+    const url = `${ROOT_URL}/query`
+        const objValues = {}
+        switch (type) {
+            case 'memoryUsage':
+                pool.getConnection(function (err, conn) {
+                    if (err) throw err
+                    conn.query(
+                        'select * from mem_util order by id desc limit 1;',
+                        function (error, results) {
+                            if (error) throw error
+                            const memUsage = results[0].value
+                            if (memUsage >= 50.0 && memUsage <= 70.0) {
+                                const notif = {
+                                    notification: {
+                                        title: "[Warning] Server high memory usage",
+                                        body: `Memory usage is at ${memUsage}%`
+                                    },
+                                    topic: "all"
+                                }
+                                admin.messaging().send(notif)
+                            }
+                            else if(memUsage > 70.0) {
+                                const notif = {
+                                    notification: {
+                                        title: "[Critical] Server high memory usage",
+                                        body: `Memory usage is at ${memUsage}%`
+                                    },
+                                    topic: "all"
+                                }
+                                admin.messaging().send(notif)
+                            }
+                        }
+                    )
+                })
+                break;
+            case 'diskUsage':
+                const diskUsage = await axios.get(url, {params: {query: 'round(100 - ((node_filesystem_avail_bytes{mountpoint="/",fstype!="rootfs"} * 100) / node_filesystem_size_bytes{mountpoint="/",fstype!="rootfs"}))'}})
+                diskUsage.data.data.result.map((data) => {
+                    objValues.value = data.value[1]
+                })
+                const diskValues = Object.values(objValues)
+                const usageValue = diskValues[0]
+                if (usageValue >= 50.0 && usageValue <= 70.0) {
+                    const notif = {
+                        notification: {
+                            title: "[Warning] Server high disk usage",
+                            body: `Memory usage is at ${usageValue}%`
+                        },
+                        topic: "all"
+                    }
+                    admin.messaging().send(notif)
+                } else if (usageValue > 70.0) {
+                    const notif = {
+                        notification: {
+                            title: "[Citical] Server high disk usage",
+                            body: `Memory usage is at ${usageValue}%`
+                        },
+                        topic: "all"
+                    }
+                    admin.messaging().send(notif)
+                }
+                break;
+            case 'cpuUsage':
+                pool.getConnection(function (err, conn) {
+                    if (err) throw err
+                    conn.query(
+                        'select * from cpu_util order by id desc limit 1;',
+                        function (error, results) {
+                            if (error) throw error
+                            const cpuUsage = results[0].value
+                            if (cpuUsage >= 30.0 && cpuUsage <= 70.0) {
+                                const notif = {
+                                    notification: {
+                                        title: "[Warning] Server high CPU usage",
+                                        body: `CPU usage is at ${cpuUsage}%`
+                                    },
+                                    topic: "all"
+                                }
+                                admin.messaging().send(notif)
+                            }
+                            else if(cpuUsage > 70.0) {
+                                const notif = {
+                                    notification: {
+                                        title: "[Critical] Server high CPU usage",
+                                        body: `CPU usage is at ${cpuUsage}%`
+                                    },
+                                    topic: "all"
+                                }
+                                admin.messaging().send(notif)
+                            }
+                        }
+                    )
+                })
+                break;
+            case 'serviceDown':
+                try {
+                    const tempApiResponse = []
+                    const apiResponse = []
+                    const instanceStatus = await axios.get(url, {params: {query: 'up == 0'}})
+                    instanceStatus.data.data.result.map((data) => {
+                        tempApiResponse.push({data})
+                    })
+                    let mergeObj = []
+                    for(item in tempApiResponse) {
+                        const unixTime = new Date(tempApiResponse[item].data.value[0]*1000)
+                        apiResponse.push({
+                            job: tempApiResponse[item].data.metric.job,
+                            status: [{
+                                instance: tempApiResponse[item].data.metric.instance,
+                                value: tempApiResponse[item].data.value[1]
+                            }]
+                        })
+                    }
+                    mergeObj = apiResponse.reduce((obj, item) => {
+                        obj[item.job] ? obj[item.job].status.push(...item.status) : (obj[item.job] = { ...item })
+                        return obj
+                    }, {})
+                    const finalObj = Object.values(mergeObj)
+                    const notif = {
+                        notification: {
+                            title: `[Critical] ${finalObj.length} server instances down`,
+                            body: `You may need to check the instance on the server`
+                        },
+                        topic: "all"
+                    }
+                    admin.messaging().send(notif)
+                } catch (error) {
+                    // for prod
+                    // if (error.message == "connect ECONNREFUSED 134.209.98.161:9090") {
+                    // for test
+                    if (error.message == "connect ECONNREFUSED 127.0.0.1:9090") {
+                        const notif = {
+                            notification: {
+                                title: `[Critical] Error connecting to Prometheus`,
+                                body: `Server can't connect to the Prometheus`
+                            },
+                            topic: "all"
+                        }
+                        admin.messaging().send(notif)
+                    }
+                }
+                break;
+        }
+}
+
+module.exports = notifScraper
