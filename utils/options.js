41c45fa4c9d70f8b3300a8399ebd0fff61ccdb25
StanceFreak <jombor.army67@gmail.com>
2024-05-24 15:46:17 +0700

    changes[target url]: forgot to add the http protocol in the url

--- utils/options.js
+++ utils/options.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 3100,
     //ROOT_URL: "http://localhost:9090/api/v1"
-    ROOT_URL: "128.199.135.220:9090/api/v1"
+    ROOT_URL: "http://128.199.135.220:9090/api/v1"
 }
