8aa4249dd3114105b043aa596b03eed56e18f6d0
StanceFreak <jombor.army67@gmail.com>
2024-06-02 04:50:35 +0700

    changes[treshold]: changed the notif treshold because the testing is done

--- scrapers/scrapers-notification.js
+++ scrapers/scrapers-notification.js
@@ -24,7 +24,7 @@ async function getMemoryAlert() {
                 function (error, results) {
                     if (error) throw error
                     const memUsage = results[0].value
-                    if (memUsage >= 10.0 && memUsage <= 70.0) {
+                    if (memUsage >= 50.0 && memUsage <= 70.0) {
                         conn.query(
                             "SELECT * FROM device_tokens;",
                             function (errorToken, resultsToken) {
@@ -102,7 +102,7 @@ async function getDiskAlert() {
         })
         const diskValues = Object.values(objValues)
         const usageValue = diskValues[0]
-        if (usageValue >= 10.0 && usageValue <= 70.0) {
+        if (usageValue >= 50.0 && usageValue <= 70.0) {
             conn.query(
                 "SELECT * FROM device_tokens;",
                 function (errorToken, resultsToken) {
@@ -174,7 +174,7 @@ async function getCpuAlert() {
                 function (error, results) {
                     if (error) throw error
                     const cpuUsage = results[0].value
-                    if (cpuUsage >= 10.0 && cpuUsage <= 70.0) {
+                    if (cpuUsage >= 50.0 && cpuUsage <= 70.0) {
                         conn.query(
                             "SELECT * FROM device_tokens;",
                             function (errorToken, resultsToken) {
